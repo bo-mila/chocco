b86af6343e689b6bab04eedb8aa673b85dafd819
Mila Bozhevskaya <mi.bozhev@gmail.com>
2021-05-18 17:02:52 +0300

    add yandex API card

--- js/main.js
+++ js/main.js
@@ -912,5 +912,106 @@ $(document).ready(() => {
   });
 
 
+
+
+
+
+
+
+
+
+
+
+
+  
+  
+  
+  //////////  YANDEX КАРТЫ ////////////////////
+  
+  ymaps.ready(init);
+  var placemarks = [
+    {
+      latitude: 59.97,
+      longitude: 30.31,
+      hintContent: '<div class="map__hint">ул.Литераторов, д. 19</div>',
+      balloonContent: [
+        '<div class="map__balloon">',
+        '<img class="map__chocco-image" src="../img/content/boun.png" alt="Батончики"/>',
+        'Самые вкусные батончики у нас! Заходите по адресу: ул.Литераторов, д.19',
+        '</div>'
+      ]
+    },
+    {
+      latitude: 59.94,
+      longitude: 30.25,
+      hintContent: '<div class="map__hint">Малый проспект В О, д.64</div>',
+      balloonContent: [
+        '<div class="map__balloon">',
+        '<img class="map__chocco-image" src="../img/content/boun.png" alt="Батончики"/>',
+        'Самые вкусные батончики у нас! Заходите по адресу: Малый проспект В О, д.64',
+        '</div>'
+      ]
+    },
+    {
+      latitude: 59.93,
+      longitude: 30.34,
+      hintContent: '<div class="map__hint">наб.реки Фонтанки, д.56</div>',
+      balloonContent: [
+        '<div class="map__balloon">',
+        '<img class="map__chocco-image" src="../img/content/boun.png" alt="Батончики"/>',
+        'Самые вкусные батончики у нас! Заходите по адресу: наб.реки Фонтанки, д.56',
+        '</div>'
+      ]
+    },
+    {
+      latitude: 59.94,
+      longitude: 30.46,
+      hintContent: '<div class="map__hint">наб.реки Фонтанки, д.56</div>',
+      balloonContent: [
+        '<div class="map__balloon">',
+        '<img class="map__chocco-image" src="../img/content/boun.png" alt="Батончики"/>',
+        'Самые вкусные батончики у нас! Заходите по адресу: наб.реки Фонтанки, д.56',
+        '</div>'
+      ]
+    }
+  ];
+  
+  function init() {
+    var map = new ymaps.Map("map", {
+      center: [59.94, 30.32],
+      zoom: 11,
+      controls: ['zoomControl'],
+      behaviors: ['drag']
+    });
+    var geoObjects = [];
+    for (let i = 0; i <placemarks.length; i++) {
+      geoObjects[i] = new ymaps.Placemark(
+        [placemarks[i].latitude, placemarks[i].longitude],
+        {
+          hintContent: placemarks[i].hintContent,
+          balloonContent: placemarks[i].balloonContent.join('')
+        },
+        {
+          iconLayout: 'default#image',
+          iconImageHref: '../img/icons/marker.svg',
+          iconImageSize: [46,57],
+          iconImageOffset: [-23, -58]
+        }
+      );
+    }
+    var clusterer = new ymaps.Clusterer({
+      clusterIcons: [
+        {
+          href: '../img/content/boun.png',
+          size: [100,100],
+          offset: [-50,-50]
+        }
+      ],
+      clusterIconContentLayout: null
+    });
+    map.geoObjects.add(clusterer);
+    clusterer.add(geoObjects);
+  }
+
   
 })()
